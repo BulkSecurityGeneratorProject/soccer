a6fed188b71a68fcc22a01a5d62770f92bd238b3
magixsource <magixsource.cn@gmail.com>
2016-09-21 20:39:42 +0800

    [A] use angular-simple-grid to show result data list

--- src/main/webapp/app/app.module.js
+++ src/main/webapp/app/app.module.js
@@ -15,7 +15,8 @@
             'infinite-scroll',
             // jhipster-needle-angularjs-add-module JHipster will add new module here
             'angular-loading-bar',
-            'isteven-multi-select'
+            'isteven-multi-select',
+            'simpleGrid'
         ])
         .run(run);
 
