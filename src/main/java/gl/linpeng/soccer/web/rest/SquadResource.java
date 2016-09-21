0027bd86722ddb5c60486c6a0f179fdf6ab7a434
magixsource <magixsource.cn@gmail.com>
2016-09-21 21:15:29 +0800

    [M] use result-data-squad service to get all exist match data and display by angular-simple-grid

--- src/main/java/gl/linpeng/soccer/web/rest/SquadResource.java
+++ src/main/java/gl/linpeng/soccer/web/rest/SquadResource.java
@@ -1,23 +1,32 @@
 package gl.linpeng.soccer.web.rest;
 
-import com.codahale.metrics.annotation.Timed;
+import gl.linpeng.soccer.domain.ResultData;
 import gl.linpeng.soccer.domain.Squad;
-
+import gl.linpeng.soccer.domain.SquadPlayer;
+import gl.linpeng.soccer.repository.ResultDataRepository;
 import gl.linpeng.soccer.repository.SquadRepository;
 import gl.linpeng.soccer.web.rest.util.HeaderUtil;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
-import org.springframework.http.HttpHeaders;
-import org.springframework.http.HttpStatus;
-import org.springframework.http.MediaType;
-import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.*;
 
-import javax.inject.Inject;
 import java.net.URI;
 import java.net.URISyntaxException;
 import java.util.List;
 import java.util.Optional;
+
+import javax.inject.Inject;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.data.domain.Example;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.codahale.metrics.annotation.Timed;
 
 /**
  * REST controller for managing Squad.
@@ -26,106 +35,143 @@ import java.util.Optional;
 @RequestMapping("/api")
 public class SquadResource {
 
-    private final Logger log = LoggerFactory.getLogger(SquadResource.class);
-        
-    @Inject
-    private SquadRepository squadRepository;
+	private final Logger log = LoggerFactory.getLogger(SquadResource.class);
 
-    /**
-     * POST  /squads : Create a new squad.
-     *
-     * @param squad the squad to create
-     * @return the ResponseEntity with status 201 (Created) and with body the new squad, or with status 400 (Bad Request) if the squad has already an ID
-     * @throws URISyntaxException if the Location URI syntax is incorrect
-     */
-    @RequestMapping(value = "/squads",
-        method = RequestMethod.POST,
-        produces = MediaType.APPLICATION_JSON_VALUE)
-    @Timed
-    public ResponseEntity<Squad> createSquad(@RequestBody Squad squad) throws URISyntaxException {
-        log.debug("REST request to save Squad : {}", squad);
-        if (squad.getId() != null) {
-            return ResponseEntity.badRequest().headers(HeaderUtil.createFailureAlert("squad", "idexists", "A new squad cannot already have an ID")).body(null);
-        }
-        Squad result = squadRepository.save(squad);
-        return ResponseEntity.created(new URI("/api/squads/" + result.getId()))
-            .headers(HeaderUtil.createEntityCreationAlert("squad", result.getId().toString()))
-            .body(result);
-    }
+	@Inject
+	private SquadRepository squadRepository;
+	@Inject
+	private ResultDataRepository resultDataRepository;
 
-    /**
-     * PUT  /squads : Updates an existing squad.
-     *
-     * @param squad the squad to update
-     * @return the ResponseEntity with status 200 (OK) and with body the updated squad,
-     * or with status 400 (Bad Request) if the squad is not valid,
-     * or with status 500 (Internal Server Error) if the squad couldnt be updated
-     * @throws URISyntaxException if the Location URI syntax is incorrect
-     */
-    @RequestMapping(value = "/squads",
-        method = RequestMethod.PUT,
-        produces = MediaType.APPLICATION_JSON_VALUE)
-    @Timed
-    public ResponseEntity<Squad> updateSquad(@RequestBody Squad squad) throws URISyntaxException {
-        log.debug("REST request to update Squad : {}", squad);
-        if (squad.getId() == null) {
-            return createSquad(squad);
-        }
-        Squad result = squadRepository.save(squad);
-        return ResponseEntity.ok()
-            .headers(HeaderUtil.createEntityUpdateAlert("squad", squad.getId().toString()))
-            .body(result);
-    }
+	/**
+	 * POST /squads : Create a new squad.
+	 *
+	 * @param squad
+	 *            the squad to create
+	 * @return the ResponseEntity with status 201 (Created) and with body the
+	 *         new squad, or with status 400 (Bad Request) if the squad has
+	 *         already an ID
+	 * @throws URISyntaxException
+	 *             if the Location URI syntax is incorrect
+	 */
+	@RequestMapping(value = "/squads", method = RequestMethod.POST, produces = MediaType.APPLICATION_JSON_VALUE)
+	@Timed
+	public ResponseEntity<Squad> createSquad(@RequestBody Squad squad)
+			throws URISyntaxException {
+		log.debug("REST request to save Squad : {}", squad);
+		if (squad.getId() != null) {
+			return ResponseEntity
+					.badRequest()
+					.headers(
+							HeaderUtil.createFailureAlert("squad", "idexists",
+									"A new squad cannot already have an ID"))
+					.body(null);
+		}
+		Squad result = squadRepository.save(squad);
+		return ResponseEntity
+				.created(new URI("/api/squads/" + result.getId()))
+				.headers(
+						HeaderUtil.createEntityCreationAlert("squad", result
+								.getId().toString())).body(result);
+	}
 
-    /**
-     * GET  /squads : get all the squads.
-     *
-     * @return the ResponseEntity with status 200 (OK) and the list of squads in body
-     */
-    @RequestMapping(value = "/squads",
-        method = RequestMethod.GET,
-        produces = MediaType.APPLICATION_JSON_VALUE)
-    @Timed
-    public List<Squad> getAllSquads() {
-        log.debug("REST request to get all Squads");
-        List<Squad> squads = squadRepository.findAll();
-        return squads;
-    }
+	/**
+	 * PUT /squads : Updates an existing squad.
+	 *
+	 * @param squad
+	 *            the squad to update
+	 * @return the ResponseEntity with status 200 (OK) and with body the updated
+	 *         squad, or with status 400 (Bad Request) if the squad is not
+	 *         valid, or with status 500 (Internal Server Error) if the squad
+	 *         couldnt be updated
+	 * @throws URISyntaxException
+	 *             if the Location URI syntax is incorrect
+	 */
+	@RequestMapping(value = "/squads", method = RequestMethod.PUT, produces = MediaType.APPLICATION_JSON_VALUE)
+	@Timed
+	public ResponseEntity<Squad> updateSquad(@RequestBody Squad squad)
+			throws URISyntaxException {
+		log.debug("REST request to update Squad : {}", squad);
+		if (squad.getId() == null) {
+			return createSquad(squad);
+		}
+		Squad result = squadRepository.save(squad);
+		return ResponseEntity
+				.ok()
+				.headers(
+						HeaderUtil.createEntityUpdateAlert("squad", squad
+								.getId().toString())).body(result);
+	}
 
-    /**
-     * GET  /squads/:id : get the "id" squad.
-     *
-     * @param id the id of the squad to retrieve
-     * @return the ResponseEntity with status 200 (OK) and with body the squad, or with status 404 (Not Found)
-     */
-    @RequestMapping(value = "/squads/{id}",
-        method = RequestMethod.GET,
-        produces = MediaType.APPLICATION_JSON_VALUE)
-    @Timed
-    public ResponseEntity<Squad> getSquad(@PathVariable Long id) {
-        log.debug("REST request to get Squad : {}", id);
-        Squad squad = squadRepository.findOne(id);
-        return Optional.ofNullable(squad)
-            .map(result -> new ResponseEntity<>(
-                result,
-                HttpStatus.OK))
-            .orElse(new ResponseEntity<>(HttpStatus.NOT_FOUND));
-    }
+	/**
+	 * GET /squads : get all the squads.
+	 *
+	 * @return the ResponseEntity with status 200 (OK) and the list of squads in
+	 *         body
+	 */
+	@RequestMapping(value = "/squads", method = RequestMethod.GET, produces = MediaType.APPLICATION_JSON_VALUE)
+	@Timed
+	public List<Squad> getAllSquads() {
+		log.debug("REST request to get all Squads");
+		List<Squad> squads = squadRepository.findAll();
+		return squads;
+	}
 
-    /**
-     * DELETE  /squads/:id : delete the "id" squad.
-     *
-     * @param id the id of the squad to delete
-     * @return the ResponseEntity with status 200 (OK)
-     */
-    @RequestMapping(value = "/squads/{id}",
-        method = RequestMethod.DELETE,
-        produces = MediaType.APPLICATION_JSON_VALUE)
-    @Timed
-    public ResponseEntity<Void> deleteSquad(@PathVariable Long id) {
-        log.debug("REST request to delete Squad : {}", id);
-        squadRepository.delete(id);
-        return ResponseEntity.ok().headers(HeaderUtil.createEntityDeletionAlert("squad", id.toString())).build();
-    }
+	/**
+	 * GET /squads/:id : get the "id" squad.
+	 *
+	 * @param id
+	 *            the id of the squad to retrieve
+	 * @return the ResponseEntity with status 200 (OK) and with body the squad,
+	 *         or with status 404 (Not Found)
+	 */
+	@RequestMapping(value = "/squads/{id}", method = RequestMethod.GET, produces = MediaType.APPLICATION_JSON_VALUE)
+	@Timed
+	public ResponseEntity<Squad> getSquad(@PathVariable Long id) {
+		log.debug("REST request to get Squad : {}", id);
+		Squad squad = squadRepository.findOne(id);
+		return Optional.ofNullable(squad)
+				.map(result -> new ResponseEntity<>(result, HttpStatus.OK))
+				.orElse(new ResponseEntity<>(HttpStatus.NOT_FOUND));
+	}
+
+	/**
+	 * DELETE /squads/:id : delete the "id" squad.
+	 *
+	 * @param id
+	 *            the id of the squad to delete
+	 * @return the ResponseEntity with status 200 (OK)
+	 */
+	@RequestMapping(value = "/squads/{id}", method = RequestMethod.DELETE, produces = MediaType.APPLICATION_JSON_VALUE)
+	@Timed
+	public ResponseEntity<Void> deleteSquad(@PathVariable Long id) {
+		log.debug("REST request to delete Squad : {}", id);
+		squadRepository.delete(id);
+		return ResponseEntity
+				.ok()
+				.headers(
+						HeaderUtil.createEntityDeletionAlert("squad",
+								id.toString())).build();
+	}
+
+	/**
+	 * GET /squads : get all the result data of squad.
+	 *
+	 * @return the ResponseEntity with status 200 (OK) and the list of result
+	 *         data in body
+	 */
+	@RequestMapping(value = "/squads/{id}/result-data", method = RequestMethod.GET, produces = MediaType.APPLICATION_JSON_VALUE)
+	@Timed
+	public List<ResultData> getAllSquadResultData(@PathVariable Long id) {
+		log.debug("REST request to get all Squad result data {}", id);
+		ResultData example = new ResultData();
+		SquadPlayer exampleSquadPlayer = new SquadPlayer();
+		Squad exampleSquad = new Squad();
+		exampleSquad.setId(id);
+		exampleSquadPlayer.setSquad(exampleSquad);
+		example.setSquadPlayer(exampleSquadPlayer);
+		List<ResultData> resultDatas = resultDataRepository.findAll(Example
+				.of(example));
+		return resultDatas;
+	}
 
 }
--- src/main/webapp/app/entities/result-data/result-data-list.controller.js
+++ src/main/webapp/app/entities/result-data/result-data-list.controller.js
@@ -5,9 +5,9 @@
         .module('soccerApp')
         .controller('ResultDataListEditController', ResultDataListEditController);
 
-    ResultDataListEditController.$inject = ['$scope','$state','Game','GameSquadQuery','ResultField'];
+    ResultDataListEditController.$inject = ['$scope','$state','Game','GameSquadQuery','ResultField','ResultDataSquad'];
 
-    function ResultDataListEditController($scope,$state,Game,GameSquadQuery,ResultField) {
+    function ResultDataListEditController($scope,$state,Game,GameSquadQuery,ResultField,ResultDataSquad) {
         var vm = this;
 
         // 1. Get game information
@@ -21,36 +21,48 @@
          	};
              GameSquadQuery.query(data,function(result){
      				vm.players = result;
-     				// translate
-     				angular.forEach(vm.players,function(player,index){
-     					player.playerName = player.player.name;
-     				});
-     				
-     				// 3. Get all result field
-     				ResultField.query(function(result){
-     					vm.resultFields = result;
-     					
-     					var basicColumns = [
-								{field:'id',disabled:true,hidden:true},
-								{field: 'playerName', disabled: true,"title": "姓名"},
-								{field:'playerNumber',inputType: 'number','title':'号码'},
-								{field:'isSubstitute',inputType: 'checkbox','title':'替补?'}
-     					];
-     					
-     					angular.forEach(vm.resultFields,function(resultField,index){
-     						basicColumns = basicColumns.concat([{field:resultField.id,title:resultField.name,inputType: 'number'}]);
-     					});
-     					
-     					vm.myGridConfig = {
-     			        	    // should return your data (an array)        
-     			        	    getData: function () { return vm.players; }, 
+     				// Get all exists result data of this squad
+     				ResultDataSquad.query({id:vm.players[0].squad.id},function(result){
+     					vm.resultDatas = result;
+     					// translate
+         				angular.forEach(vm.players,function(player,index){
+         					player.playerName = player.player.name;
+         					
+         					angular.forEach(vm.resultDatas,function(resultData,index){
+         						if(player.player.id == resultData.squadPlayer.player.id){
+             						player[""+resultData.resultField.id] = resultData.value;
+             					}
+         					});
+         					
+         				});
+         				
+         				// 3. Get all result field
+         				ResultField.query(function(result){
+         					vm.resultFields = result;
+         					
+         					var columns = [
+    								{field:'id',disabled:true,hidden:true},
+    								{field: 'playerName', disabled: true,"title": "姓名"},
+    								{field:'playerNumber',inputType: 'number','title':'号码'},
+    								{field:'isSubstitute',inputType: 'checkbox','title':'替补'}
+         					];
+         					
+         					angular.forEach(vm.resultFields,function(resultField,index){
+         						columns = columns.concat([{field:resultField.id,title:resultField.name,inputType: 'number'}]);
+         					});
+         					
+         					vm.myGridConfig = {
+         			        	    // should return your data (an array)        
+         			        	    getData: function () { return vm.players; }, 
 
-     			        	    options: { 
-     			        	        showDelete: true,
-     			        	        columns: basicColumns
-     			        	    }
-     			        	};
-     					
+         			        	    options: { 
+         			        	        showDelete: true,
+         			        	        columns: columns
+         			        	    }
+         			        	};
+         					
+         				});
+         				
      				});
      				
      		});
