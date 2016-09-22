de47c8e021f678e67bb9691c36f0dac726469596
magixsource <magixsource.cn@gmail.com>
2016-09-22 22:32:38 +0800

    [R] refactory result-data save

--- src/main/webapp/app/entities/result-data/result-data-list.controller.js
+++ src/main/webapp/app/entities/result-data/result-data-list.controller.js
@@ -14,7 +14,6 @@
         // 1. Get game information
          Game.get({id:$state.params.id},function(result){
         	 vm.game = result;
-        	 
         	// 2. Get players information
              var data = {
          			id:$state.params.id,
@@ -24,118 +23,77 @@
           			id:$state.params.id,
           			tid: vm.game.roadTeam.id
           	};
-             GameSquadQuery.query(data,function(result){
-     				vm.players = result;
-     				// Get all exists result data of this squad
-     				ResultDataSquad.query({id:vm.players[0].squad.id},function(result){
-     					vm.resultDatas = result;
-     					// translate
-         				angular.forEach(vm.players,function(player,index){
-         					player.playerName = player.player.name;
-         					
-         					angular.forEach(vm.resultDatas,function(resultData,index){
-         						if(player.player.id == resultData.squadPlayer.player.id){
-             						player[""+resultData.resultField.id] = resultData.value;
-             						player['$'+resultData.resultField.id] = resultData.id;
-             					}
-         					});
-         					
-         				});
-         				
-         				// 3. Get all result field
-         				ResultField.query(function(result){
-         					vm.resultFields = result;
-         					
-         					var columns = [
-    								{field:'id',disabled:true,hidden:true},
-    								{field: 'playerName', disabled: true,"title": "姓名"},
-    								{field:'playerNumber',inputType: 'number','title':'号码'},
-    								{field:'isSubstitute',inputType: 'checkbox','title':'替补'}
-         					];
-         					
-         					angular.forEach(vm.resultFields,function(resultField,index){
-         						columns = columns.concat([{field:resultField.id,title:resultField.name,inputType: 'number'}]);
-         					});
-         					
-         					vm.myGridConfig = {
-         			        	    // should return your data (an array)        
-         			        	    getData: function () { return vm.players; }, 
-
-         			        	    options: { 
-         			        	        showDeleteButton: false,
-         			        	        showEditButton: true,
-         			        	        editable: true,
-         			        	        disabled:false,
-         			        	        perRowEditModeEnabled: true,
-         			        	        allowMultiSelect: false,
-         			        	        dynamicColumns: true,
-         			        	        editRequested:vm.save,
-         			        	        columns: columns
-         			        	    }
-         			        	};
-         					
-         				});
-         				
-     				});
-     				
-     		});// End of home data load
              
-             // road data load
-             GameSquadQuery.query(roadData,function(result){
-  				vm.roadPlayers = result;
-  				// Get all exists result data of this squad
-  				ResultDataSquad.query({id:vm.roadPlayers[0].squad.id},function(result){
-  					vm.roadResultDatas = result;
-  					// translate
-      				angular.forEach(vm.roadPlayers,function(player,index){
-      					player.playerName = player.player.name;
-      					
-      					angular.forEach(vm.roadResultDatas,function(resultData,index){
-      						if(player.player.id == resultData.squadPlayer.player.id){
-          						player[""+resultData.resultField.id] = resultData.value;
-          						player['$'+resultData.resultField.id] = resultData.id;
-          					}
-      					});
-      					
-      				});
-      				
-      				// 3. Get all result field
-      				ResultField.query(function(result){
-      					vm.resultFields = result;
-      					
-      					var columns = [
- 								{field:'id',disabled:true,hidden:true},
- 								{field: 'playerName', disabled: true,"title": "姓名"},
- 								{field:'playerNumber',inputType: 'number','title':'号码'},
- 								{field:'isSubstitute',inputType: 'checkbox','title':'替补'}
-      					];
-      					
-      					angular.forEach(vm.resultFields,function(resultField,index){
-      						columns = columns.concat([{field:resultField.id,title:resultField.name,inputType: 'number'}]);
-      					});
-      					
-      					vm.myRoadGridConfig = {
-      			        	    // should return your data (an array)        
-      			        	    getData: function () { return vm.roadPlayers; }, 
-
-      			        	    options: { 
-      			        	        showDeleteButton: false,
-      			        	        showEditButton: true,
-      			        	        editable: true,
-      			        	        disabled:false,
-      			        	        perRowEditModeEnabled: true,
-      			        	        allowMultiSelect: false,
-      			        	        dynamicColumns: true,
-      			        	        editRequested:vm.save,
-      			        	        columns: columns
-      			        	    }
-      			        	};
-      					
-      				});
-      				
-  				});
-  				
+             // new style
+             vm.players = [];
+             vm.resultDatas = [];
+             vm.roadPlayers = [];
+             vm.roadResultDatas = [];
+             vm.resultFields = [];
+             
+            getGridConfig(data,vm.players,vm.resultDatas,vm.resultFields,function(result){
+            	 vm.myGridConfig = result;
              });
+            getGridConfig(roadData,vm.roadPlayers,vm.roadResultDatas,vm.resultFields,function(result){
+            	vm.myRoadGridConfig = result;
+            });
+             
+             function copy(src,dist){
+            	 angular.forEach(src,function(obj,idx){
+            		 dist.push(obj);
+        		 });
+             }
+             
+             function getGridConfig(params,players,datas,fields,callback){
+            	 GameSquadQuery.query(params,function(result){
+            		 copy(result,players);
+            		 ResultDataSquad.query({id:players[0].squad.id},function(result){
+            			 copy(result,datas);
+	   					// translate
+	       				angular.forEach(players,function(player,index){
+	       					player.playerName = player.player.name;
+	       					angular.forEach(datas,function(resultData,index){
+	       						if(player.player.id == resultData.squadPlayer.player.id){
+	           						player[""+resultData.resultField.id] = resultData.value;
+	           						player['$'+resultData.resultField.id] = resultData.id;
+	           					}
+	       					});
+	       				});
+	       				ResultField.query(function(result){
+	       					if(!fields || fields.length ==0){
+	       						copy(result,fields);
+	       					}
+	       					
+	       					var columns = [
+   								{field:'id',disabled:true,hidden:true},
+   								{field: 'playerName', disabled: true,"title": "姓名"},
+   								{field:'playerNumber',inputType: 'number','title':'号码'},
+   								{field:'isSubstitute',inputType: 'checkbox','title':'替补'}
+   							];
+   		       				angular.forEach(fields,function(resultField,index){
+   		  						columns = columns.concat([{field:resultField.id,title:resultField.name,inputType: 'number'}]);
+   		  					});
+   		       				var result = {
+   					        	    getData: function () { return players; }, 
+   					        	    options: { 
+   					        	        showDeleteButton: false,
+   					        	        showEditButton: true,
+   					        	        editable: true,
+   					        	        disabled:false,
+   					        	        perRowEditModeEnabled: true,
+   					        	        allowMultiSelect: false,
+   					        	        dynamicColumns: true,
+   					        	        editRequested:vm.save,
+   					        	        columns: columns
+   					        	    }
+   					        	};
+   		       				callback(result);
+	       				});
+	       				
+            		 });// end of ResultDataSquad query
+            	 });// end of GameSquadQuery query
+             }
+             
          });
          
          function saveRow(row){
