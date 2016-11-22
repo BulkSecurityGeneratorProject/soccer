2ca5c8901e729fc415b7516da726e263aa9f03aa
magixsource <magixsource.cn@gmail.com>
2016-11-22 13:49:45 +0800

    [m] public club player and matches info

--- src/main/webapp/app/public/club/public.club.controller.js
+++ src/main/webapp/app/public/club/public.club.controller.js
@@ -3,11 +3,21 @@
 
     angular
         .module('soccerApp')
-        .controller('PublicClubController', PublicClubController);
+        .controller('PublicClubController', PublicClubController)
+        .filter('ageFilter', function() {
+            function calculateAge(birthday) { // birthday is a date
+                var ageDifMs = Date.now() - birthday.getTime();
+                var ageDate = new Date(ageDifMs); // miliseconds from epoch
+                return Math.abs(ageDate.getUTCFullYear() - 1970);
+            }
+            return function(birthdate) {
+                  return calculateAge(new Date(birthdate));
+            }; 
+       });
 
-    PublicClubController.$inject = ['$scope','$state',  'Club','TeamPlayer'];
+    PublicClubController.$inject = ['$scope','$state',  'Club','TeamPlayer','TeamGame'];
 
-    function PublicClubController ($scope, $state,Club,TeamPlayer) {
+    function PublicClubController ($scope, $state,Club,TeamPlayer,TeamGame) {
     	var clubId = 1;
     	
         var vm = this;
@@ -30,6 +40,13 @@
         $scope.isActiveTab = function(tabUrl) {
             return tabUrl == $scope.currentTab;
         }
+        $scope.now = new Date();
+        
+        $scope.isBefore = function(dStr) {
+            return $scope.now.getTime() > new Date(dStr).getTime();
+        }
+        
+        
         
         // 1. Get Club Basic information
         // vm.club = Club.Get({id : $state.params.id});
@@ -37,7 +54,10 @@
         // 2. Get All Club team with player
         vm.players = TeamPlayer.query({id:1});
         // 3. Get schdule of teams
-        
+       TeamGame.query({id:1},function(result){
+    	   vm.games = result;
+        });
         // 4. History and so on
+        
     }
 })();
