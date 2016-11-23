cb3d1aa04897b33576cf9cb24b079a58cadcb402
magixsource <magixsource.cn@gmail.com>
2016-11-23 15:32:29 +0800

    [a] add find games by team method,to query team games as schduled

--- src/main/java/gl/linpeng/soccer/repository/GameRepository.java
+++ src/main/java/gl/linpeng/soccer/repository/GameRepository.java
@@ -2,14 +2,26 @@ package gl.linpeng.soccer.repository;
 
 import gl.linpeng.soccer.domain.Game;
 
-import org.springframework.data.jpa.repository.*;
-
 import java.util.List;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 
 /**
  * Spring Data JPA repository for the Game entity.
  */
 @SuppressWarnings("unused")
-public interface GameRepository extends JpaRepository<Game,Long> {
+public interface GameRepository extends JpaRepository<Game, Long> {
 
+	/**
+	 * Get all games by team,either home or road team
+	 * 
+	 * @param id
+	 *            team id
+	 * @param recent
+	 *            recent count
+	 * @return
+	 */
+	@Query("select g from Game g where (g.homeTeam.id = ?#{[0]} or g.roadTeam.id=?#{[0]}) and abs(datediff(day,now(),g.startAt)) <= ?#{[1]}")
+	List<Game> findGamesByTeam(Long id, int recent);
 }
