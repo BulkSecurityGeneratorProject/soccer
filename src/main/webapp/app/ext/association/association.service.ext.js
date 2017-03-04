fefc0738e5dd5ef0dcfce237ccce092c487eed67
magixsource <magixsource.cn@gmail.com>
2017-03-04 12:00:13 +0800

    [a] add association ext service

---
+++ src/main/webapp/app/ext/association/association.service.ext.js
@@ -0,0 +1,20 @@
+(function() {
+    'use strict';
+    angular
+        .module('soccerApp')
+        .factory('AssociationExt', AssociationExt);
+
+    AssociationExt.$inject = ['$resource'];
+
+    function AssociationExt ($resource) {
+        var resourceUrl =  'api/associations/:id';
+
+        return $resource(resourceUrl, {}, {
+            'queryClubs':{url: resourceUrl+'/clubs', method: 'GET', isArray: true},
+            'queryDivisions':{url: resourceUrl+'/divisions', method: 'GET', isArray: true},
+            'queryPlayers':{url: resourceUrl+'/players', method: 'GET', isArray: true},
+            'queryDivisionevents':{url: resourceUrl+'/division-events', method: 'GET', isArray: true},
+            'queryGames':{url: resourceUrl+'/games', method: 'GET', isArray: true}
+        });
+    }
+})();
