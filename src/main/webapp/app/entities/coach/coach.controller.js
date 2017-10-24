6732ed47c432bc5b3bd940deeea26188a876251b
magixsource <magixsource.cn@gmail.com>
2017-10-24 20:23:55 +0800

    [m] add pagination to Coach

--- src/main/webapp/app/entities/coach/coach.controller.js
+++ src/main/webapp/app/entities/coach/coach.controller.js
@@ -5,18 +5,54 @@
         .module('soccerApp')
         .controller('CoachController', CoachController);
 
-    CoachController.$inject = ['$scope', '$state', 'Coach'];
+    CoachController.$inject = ['$scope', '$state', 'Coach', 'ParseLinks', 'AlertService', 'pagingParams', 'paginationConstants'];
 
-    function CoachController ($scope, $state, Coach) {
+    function CoachController ($scope, $state, Coach, ParseLinks, AlertService, pagingParams, paginationConstants) {
         var vm = this;
         
-        vm.coaches = [];
+        vm.loadPage = loadPage;
+        vm.predicate = pagingParams.predicate;
+        vm.reverse = pagingParams.ascending;
+        vm.transition = transition;
+        vm.itemsPerPage = paginationConstants.itemsPerPage;
 
         loadAll();
 
-        function loadAll() {
-            Coach.query(function(result) {
-                vm.coaches = result;
+        function loadAll () {
+            Coach.query({
+                page: pagingParams.page - 1,
+                size: vm.itemsPerPage,
+                sort: sort()
+            }, onSuccess, onError);
+            function sort() {
+                var result = [vm.predicate + ',' + (vm.reverse ? 'asc' : 'desc')];
+                if (vm.predicate !== 'id') {
+                    result.push('id');
+                }
+                return result;
+            }
+            function onSuccess(data, headers) {
+                vm.links = ParseLinks.parse(headers('link'));
+                vm.totalItems = headers('X-Total-Count');
+                vm.queryCount = vm.totalItems;
+                vm.coaches = data;
+                vm.page = pagingParams.page;
+            }
+            function onError(error) {
+                AlertService.error(error.data.message);
+            }
+        }
+
+        function loadPage (page) {
+            vm.page = page;
+            vm.transition();
+        }
+
+        function transition () {
+            $state.transitionTo($state.$current, {
+                page: vm.page,
+                sort: vm.predicate + ',' + (vm.reverse ? 'asc' : 'desc'),
+                search: vm.currentSearch
             });
         }
     }
