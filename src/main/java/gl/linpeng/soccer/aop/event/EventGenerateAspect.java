d1aa646286755a046f5b17ca9d5d9a9d6099f657
lin.peng <lin.peng@dtyunxi.com>
2017-10-26 12:05:45 +0800

    [M] scan repository and find entity before update

--- src/main/java/gl/linpeng/soccer/aop/event/EventGenerateAspect.java
+++ src/main/java/gl/linpeng/soccer/aop/event/EventGenerateAspect.java
@@ -7,6 +7,7 @@ import org.aspectj.lang.annotation.Around;
 import org.aspectj.lang.annotation.Aspect;
 import org.aspectj.lang.annotation.Pointcut;
 import org.springframework.beans.BeanUtils;
+import org.springframework.context.ApplicationContext;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.util.ReflectionUtils;
 
@@ -24,15 +25,17 @@ import java.util.Map;
 @Aspect
 public class EventGenerateAspect {
 
+    private static final String REPOSITORY_SUFFIX = "Repository";
+    private static final String METHOD_GET_ID = "getId";
+
+    @Resource
+    private ApplicationContext appContext;
+
+    private Map<String, JpaRepository> repositoryMap;
+
     @Resource
     private AssociationRepository associationRepository;
 
-    private Map<Class, JpaRepository> repositoryMap;
-
-    public EventGenerateAspect() {
-        repositoryMap = new HashMap<>();
-        repositoryMap.put(Association.class, associationRepository);
-    }
 
     @Pointcut("execution(* gl.linpeng.soccer.repository..*.save*(..))")
     public void savePointcut() {
@@ -46,24 +49,39 @@ public class EventGenerateAspect {
 
         Object objBefore = null;
         Object arg = args[0];
-        Method method = ReflectionUtils.findMethod(clz, "getId");
-        Object id = ReflectionUtils.invokeMethod(method, arg);
+        Method methodGetId = ReflectionUtils.findMethod(clz, METHOD_GET_ID);
+        Object id = ReflectionUtils.invokeMethod(methodGetId, arg);
         if (id != null) {
             // update
-            objBefore = repositoryMap.get(clz).findOne((Long) id);
+            if (null == repositoryMap) {
+                repositoryMap = new HashMap<>();
+                String[] beanNames = appContext.getBeanNamesForType(JpaRepository.class);
+                // Arrays.sort(beanNames);
+                for (String beanName : beanNames) {
+                    Object bean = appContext.getBean(beanName);
+                    if (beanName.endsWith(REPOSITORY_SUFFIX) && null != bean) {
+                        String clzName = beanName.replace(REPOSITORY_SUFFIX, "").toUpperCase();
+                        repositoryMap.put(clzName, (JpaRepository) bean);
+                    }
+                }
+            }
+            Association association = associationRepository.findOne((Long) id);
+            objBefore = repositoryMap.get(clz.getSimpleName().toUpperCase()).findOne((Long) id);
         } else {
             //create
             objBefore = clz.newInstance();
             BeanUtils.copyProperties(arg, objBefore);
         }
-
+        System.out.println("====before " + objBefore);
         // begin process
         result = joinPoint.proceed();
-        Association objAfter = (Association) result;
 
         // after process
-        if (!objAfter.getId().equals(id)) {
+        Object afterId = ReflectionUtils.invokeMethod(methodGetId, result);
+        if (!afterId.equals(id)) {
             System.out.println(">>>>>>>>>>>>>>>新增记录");
+        } else {
+            System.out.println(">>>>>>>>>>>>>>>修改记录，" + objBefore + "," + result);
         }
 
         return result;
