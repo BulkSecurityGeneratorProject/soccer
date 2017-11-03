a5e004f7c64ebcebfd08efc4ebff1ab2d9777c23
magixsource <magixsource.cn@gmail.com>
2017-11-03 23:35:35 +0800

    update home page add news

--- src/main/webapp/app/home/home.controller.js
+++ src/main/webapp/app/home/home.controller.js
@@ -5,9 +5,9 @@
         .module('soccerApp')
         .controller('HomeController', HomeController);
 
-    HomeController.$inject = ['$scope', 'Principal', 'LoginService', '$state','DivisionEventTable'];
+    HomeController.$inject = ['$scope', 'Principal', 'LoginService', '$state','DivisionEventTable','Article','AlertService'];
 
-    function HomeController ($scope, Principal, LoginService, $state,DivisionEventTable) {
+    function HomeController ($scope, Principal, LoginService, $state,DivisionEventTable,Article,AlertService) {
         var vm = this;
 
         vm.account = null;
@@ -16,13 +16,15 @@
         vm.register = register;
         // here always query divistion event 1 as first support division
         vm.divisionTable = DivisionEventTable.query({id:1});
+        vm.articles = []
 
         $scope.$on('authenticationSuccess', function() {
             getAccount();
         });
 
         getAccount();
-
+        loadAll();
+        
         function getAccount() {
             Principal.identity().then(function(account) {
                 vm.account = account;
@@ -31,6 +33,24 @@
         }
         function register () {
             $state.go('register');
+        }
+
+        function loadAll () {
+            Article.query({
+                page: 0,
+                size: 10,
+                sort: sort()
+            }, onSuccess, onError);
+            function sort() {
+                var result = ['id,asc'];
+                return result;
+            }
+            function onSuccess(data, headers) {
+                vm.articles = data;
+            }
+            function onError(error) {
+                AlertService.error(error.data.message);
+            }
         }
     }
 })();
