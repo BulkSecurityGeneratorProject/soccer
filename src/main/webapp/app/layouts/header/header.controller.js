92f1ddaad89635b922203349c8b08c6ee4c302a1
magixsource <magixsource.cn@gmail.com>
2017-11-03 15:55:42 +0800

    [u] update index

--- src/main/webapp/app/layouts/header/header.controller.js
+++ src/main/webapp/app/layouts/header/header.controller.js
@@ -5,13 +5,40 @@
         .module('soccerApp')
         .controller('HeaderController', HeaderController);
 
-    HeaderController.$inject = ['$state','Association'];
+    HeaderController.$inject = ['$state','Association','Auth', 'Principal', 'ProfileService', 'LoginService'];
 
-    function HeaderController ($state,Association) {
+    function HeaderController ($state,Association,Auth, Principal, ProfileService, LoginService) {
         var vm = this;
         vm.associations = [];
+        vm.isNavbarCollapsed = true;
+        vm.isAuthenticated = Principal.isAuthenticated;
+
+        ProfileService.getProfileInfo().then(function(response) {
+            vm.inProduction = response.inProduction;
+            vm.swaggerEnabled = response.swaggerEnabled;
+        });
         
         loadAll();
+
+        function login() {
+            collapseNavbar();
+            LoginService.open();
+        }
+
+        function logout() {
+            collapseNavbar();
+            Auth.logout();
+            $state.go('home');
+        }
+
+        function toggleNavbar() {
+            vm.isNavbarCollapsed = !vm.isNavbarCollapsed;
+        }
+
+        function collapseNavbar() {
+            vm.isNavbarCollapsed = true;
+        }
+        
 
         function loadAll() {
             Association.query(function(result) {
