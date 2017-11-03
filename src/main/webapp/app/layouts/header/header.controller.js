ad06a79767e5830fda4599de37cb3fbe2ece9797
magixsource <magixsource.cn@gmail.com>
2017-11-03 12:46:01 +0800

    [M] add association to home page

--- src/main/webapp/app/layouts/header/header.controller.js
+++ src/main/webapp/app/layouts/header/header.controller.js
@@ -5,10 +5,18 @@
         .module('soccerApp')
         .controller('HeaderController', HeaderController);
 
-    HeaderController.$inject = ['$state'];
+    HeaderController.$inject = ['$state','Association'];
 
-    function HeaderController ($state) {
+    function HeaderController ($state,Association) {
         var vm = this;
-      
+        vm.associations = [];
+        
+        loadAll();
+
+        function loadAll() {
+            Association.query(function(result) {
+                vm.associations = result;
+            });
+        }
     }
 })();
